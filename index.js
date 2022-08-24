fb92b63d1c0a040b21ffc6869e1647a5c94cbd30
tania <taniaosano15@gmail.com>
2022-08-24 14:55:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(driverA,driverB){
+
+    return driverA.filter(
+        (possibleMatch) => possibleMatch.toLowerCase() === driverB.toLowerCase()
+
+    );
+
+}
+function fuzzyMatch(driverA,driverC){
+    return driverA.filter(
+    (possibleMatch) =>possibleMatch.toLowerCase().indexOf (driverC.toLowerCase()) ===0 
+    );
+}
+function matchName(driverA,driverB){
+return driverA.filter((record) => record.name === driverB);
+}
